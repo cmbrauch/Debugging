1c2c51f1b8654074869543a7960f18c9dbc4ead6
Chris <cmbrauch00@gmail.com>
2020-06-03 16:19:58 -0400

    Add a . to .person1 in scripts.js
    co-authored-by: Mike Watts<WattsJMichael@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     const verbInput = $("input#verb").val();
     const nounInput = $("input#noun").val();
 
-    $("person1").text(person1Input);
+    $(".person1").text(person1Input);
     $(".person2").text(person2Input);
     $(".animal").text(animalInput);
     $(".exclamation").text(exclamationInput);
